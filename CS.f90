e4ca88913de5d2ab1fb551d96f9f24105a21eb26
Roman Rodionov <cermies@gmail.com>
2014-07-16 12:33:14 +0400

    comments

--- CS.f90
+++ CS.f90
@@ -5,28 +5,43 @@ module crossections
 	!
 	! getcs(cskind, energy) - returns crossection of reaction with index cskind
 	!                         at energy
-    !             	
+    ! getac(cskind, energy) - returns type(VECTOR) containing array of angular 
+	!                         distribution coefficients of cskind reaction at
+	!                         energy
+	! S U B R O U T I N E S
+	! 
+	! cs_init(filename) - initializes crossection data. filename - a name of textfile
+	!                     which contains descriprion of files with crossection data
+	! cs_destroy() - deallocates all dynamic crossection data. 
     implicit none 
 	
     private
-	public  :: cs_init, cs_destroy, getcs, getac
+	public  :: cs_init, cs_destroy, getcs, getac, type(VECTOR)
 	
 	type VECTOR
-	    integer :: N
+	    integer :: N   ! Length of V
 	    real(8), dimension(:), allocatable :: V
 	end type
 	
 	type TABLE
-	    integer :: NX, NY
+	    integer :: NX, NY ! V dimensions
 	    real(8), dimension(:, :), allocatable :: V
 	end type
 	
-    type (VECTOR), dimension(:), allocatable :: CS, E, EA
-	real(8),       dimension(:), allocatable :: QI, QM, M1, M2
-	type (TABLE),  dimension(:), allocatable :: A
+    type (VECTOR), dimension(:), allocatable :: CS, E, EA     ! E - array of reaction energies. EA - angular distribution energies
+	                                                          ! CS - crossections. Example:
+															  ! r - the number of reaction under consideration. Then
+															  ! E(r) % V - array of energies for reaction r, where crossections defined.
+															  ! CS(r) % V - array of corresponding crossections. 
+															  ! reaction r: E(r) % V(i) -> CS(r) % V(i), i = 1,...,E(r)%N
+	real(8),       dimension(:), allocatable :: QI, QM, M1, M2 ! reaction energies and product masses
+	type (TABLE),  dimension(:), allocatable :: A ! A(r) - array of coefficients of reaction r angular distribution function
+	                                              ! A(r) % V(i, j) - coefficient of number j at energy EA(r) % V(i)
 	
     contains
 	
+	    ! filename file format:
+		!
 	    subroutine cs_init(filename)
 		    character(len = *), intent(in) :: filename
 			
@@ -45,8 +60,8 @@ module crossections
 		    
 		end subroutine
 	    
-	    function getcs(cskind, energy)
-		    integer(4), intent(in) :: cskind
+	    function getcs(cskind, energy)  ! linear interpolation used here
+		    integer(4), intent(in) :: cskind   ! If energy lies outside avaible crossection data range 0.0 is returned
 			real(8),    intent(in) :: energy
 			real(8) :: getcs
 			
@@ -63,9 +78,9 @@ module crossections
 			
 		end function getcs
 		
-		function getac(cskind, energy)
-		    integer(4), intent(in) :: cskind
-		    real(8),    intent(in) :: energy
+		function getac(cskind, energy) ! linear interpolation used here. 
+		    integer(4), intent(in) :: cskind  ! If energy lies outside avaible crossection data range then
+		    real(8),    intent(in) :: energy  ! getac structure returned: getac%N=1, getac%V(1)=1.0 - uniform distribution is assumed
 		    type (VECTOR) :: getac
 		    
 		    integer :: i, j
@@ -89,6 +104,8 @@ module crossections
 		    
 		end function getac
 		
+		! search the max index in array X so X(i)<=V
+		! return 0 if V<X(1) or V>=X(N), where N - length of X
 		function search(X, V)
 		    real(8), dimension(:), intent(in) :: X
 			real(8), intent(in) :: V
@@ -96,7 +113,7 @@ module crossections
 			
 			integer(4) :: imin, imax, imid, N
 			N = size(X)
-			if (V .LE. X(1) .OR. V .GE. X(N)) then
+			if (V .LT. X(1) .OR. V .GE. X(N)) then
 			    search = 0
 			else
 			    imin = 1
