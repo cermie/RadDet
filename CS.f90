2ca468aba80a74da0a0b4fa35ef47577f83547e9
Roman Rodionov <cermies@gmail.com>
2014-07-17 14:19:47 +0400

    comments there

--- CS.f90
+++ CS.f90
@@ -40,161 +40,161 @@ module crossections
 	type (TABLE),  dimension(:), allocatable :: A ! A(r) - array of coefficients of reaction r angular distribution function
 	                                              ! A(r) % V(i, j) - coefficient of number j at energy EA(r) % V(i)
 	
-    contains
+contains
 	
-	    ! filename file format (this is testfile):
-		! first line:
-		! NR - Total number of reactions (integer)
-		! followed by NR lines of format
-		! M1 M2 QI QM 
-		! CS_FILENAME 
-		! ANG_FILENAME
-		! where M1, M2 - product masses in u (atomic mass unit)
-		!       QI, QM - reaction energy
-		!       CS_FILENAME - name of file with crossection data (80 symbols max)
-		!       CS_ANG_FILENAME - name of file with angular disttribution data (80 symbols max)
-	    subroutine cs_init(filename, error)
-		    character(len = *), intent(in) :: filename
-			integer, intent(out) :: error ! error code. 0 - if no error
-			
-			integer(4) :: i, NR, err2
-			character(len = 80) :: cs_file, ang_file
-			
-			error = 0			
-			open(11, file = filename, form = 'FORMATTED', action = 'READ')
-			read(11, *) NR
-			allocate(E(NR), EA(NR), CS(NR), QI(NR), QM(NR), M1(NR), M2(NR), A(NR))
-			do i = 1, NR
-			    read(11, *) M1(i), M2(i), QI(i), QM(i)
-				read(11, *) cs_file
-				call read_cs(cs_file, i, err2)
-				read(11, *) ang_file
-				call read_ang(ang_file, i, err2)
-			end do
-			close(11)
-			NReact = NR
-		end subroutine cs_init
+    ! filename file format (this is testfile):
+	! first line:
+	! NR - Total number of reactions (integer)
+	! followed by NR lines of format
+	! M1 M2 QI QM 
+	! CS_FILENAME 
+	! ANG_FILENAME
+	! where M1, M2 - product masses in u (atomic mass unit)
+	!       QI, QM - reaction energy
+	!       CS_FILENAME - name of file with crossection data (80 symbols max)
+	!       CS_ANG_FILENAME - name of file with angular disttribution data (80 symbols max)
+    subroutine cs_init(filename, error)
+	    character(len = *), intent(in) :: filename
+		integer, intent(out) :: error ! error code. 0 - if no error
 		
-		subroutine read_cs(filename, react, error)  ! read crossections from binary file for reaction react
-		    character(len = *), intent(in) :: filename
-			integer, intent(in)  :: react
-			integer, intent(out) :: error
-			
-			integer :: N, i
-			
-			error = 0
-			open(12, file = filename, form = 'UNFORMATTED', action = 'READ')
-			read(12) N
-			E(react) % N = N
-			CS(react) % N = N
-			allocate(E(react) % V(N), CS(react) % V(N))
-			read(12) E(react) % V
-            read(12) CS(react) % V
-			close(12)
-		end subroutine read_cs
+		integer(4) :: i, NR, err2
+		character(len = 80) :: cs_file, ang_file
 		
-		subroutine read_ang(filename, react, error) ! read angular distribution coefficients from binary file for reaction react
-		    character(len = *), intent(in) :: filename
-			integer, intent(in)  :: react
-			integer, intent(out) :: error
-			
-			integer :: NX, NY, i
-			
-			error = 0
-			open(12, file = filename, form = 'UNFORMATTED', action = 'READ')
-			read(12) NX, NY
-			EA(react) % N = NX
-			A(react) % NX = NX
-			A(react) % NY = NY
-			allocate(EA(react) % V(NX), A(react) % V(NX, NY))
-			read(12) EA(react) % V
-			read(12) A(react) % V
-			close(12)
-		end subroutine read_ang
+		error = 0			
+		open(11, file = filename, form = 'FORMATTED', action = 'READ')
+		read(11, *) NR
+		allocate(E(NR), EA(NR), CS(NR), QI(NR), QM(NR), M1(NR), M2(NR), A(NR))
+		do i = 1, NR
+		    read(11, *) M1(i), M2(i), QI(i), QM(i)
+			read(11, *) cs_file
+			call read_cs(cs_file, i, err2)
+			read(11, *) ang_file
+			call read_ang(ang_file, i, err2)
+		end do
+		close(11)
+		NReact = NR
+	end subroutine cs_init
+	
+	subroutine read_cs(filename, react, error)  ! read crossections from binary file for reaction react
+	    character(len = *), intent(in) :: filename
+		integer, intent(in)  :: react
+		integer, intent(out) :: error
 		
-		subroutine cs_destroy()
+		integer :: N, i
 		
-		    integer :: N, i
-			
-			if (.NOT. allocated(E%V)) then
-			    return
-			end if
-		    N = size(E)
-		    do i = 1, N
-		        deallocate(E(i) % V, CS(i) % V, EA(i) % V, A(i) % V)
-		    end do
-		    deallocate(E, CS, QI, QM, M1, M2, A, EA)
-		    
-		end subroutine
+		error = 0
+		open(12, file = filename, form = 'UNFORMATTED', action = 'READ')
+		read(12) N
+		E(react) % N = N
+		CS(react) % N = N
+		allocate(E(react) % V(N), CS(react) % V(N))
+		read(12) E(react) % V
+           read(12) CS(react) % V
+		close(12)
+	end subroutine read_cs
+	
+	subroutine read_ang(filename, react, error) ! read angular distribution coefficients from binary file for reaction react
+	    character(len = *), intent(in) :: filename
+		integer, intent(in)  :: react
+		integer, intent(out) :: error
+		
+		integer :: NX, NY, i
+		
+		error = 0
+		open(12, file = filename, form = 'UNFORMATTED', action = 'READ')
+		read(12) NX, NY
+		EA(react) % N = NX
+		A(react) % NX = NX
+		A(react) % NY = NY
+		allocate(EA(react) % V(NX), A(react) % V(NX, NY))
+		read(12) EA(react) % V
+		read(12) A(react) % V
+		close(12)
+	end subroutine read_ang
+	
+	subroutine cs_destroy()
+	
+	    integer :: N, i
+		
+		if (.NOT. allocated(E%V)) then
+		    return
+		end if
+	    N = size(E)
+	    do i = 1, N
+	        deallocate(E(i) % V, CS(i) % V, EA(i) % V, A(i) % V)
+	    end do
+	    deallocate(E, CS, QI, QM, M1, M2, A, EA)
 	    
-	    function getcs(cskind, energy)  ! linear interpolation used here
-		    integer(4), intent(in) :: cskind   ! If energy lies outside avaible crossection data range 0.0 value is returned
-			real(8),    intent(in) :: energy
-			real(8) :: getcs
-			
-			integer :: i
-			
-			i = search(E(cskind) % V, energy)
-			if (i .EQ. 0) then
-			    getcs = 0.0
-			else
-			    getcs = CS(cskind) % V(i) + (CS(cskind) % V(i + 1) - &
-				        CS(cskind) % V(i)) * (energy - E(cskind) % V(i)) / &
-				        (E(cskind) % V(i + 1) - E(cskind) % V(i))
-			end if
-			
-		end function getcs
+	end subroutine
+	    
+    function getcs(cskind, energy)  ! linear interpolation used here
+	    integer(4), intent(in) :: cskind   ! If energy lies outside avaible crossection data range 0.0 value is returned
+		real(8),    intent(in) :: energy
+		real(8) :: getcs
 		
-		function getac(cskind, energy) ! linear interpolation used here. 
-		    integer(4), intent(in) :: cskind  ! If energy lies outside avaible crossection data range then
-		    real(8),    intent(in) :: energy  ! getac structure returned: getac%N=1, getac%V(1)=1.0 - uniform distribution is assumed
-		    type (VECTOR) :: getac
-		    
-		    integer :: i, j
-		    real(8) :: fc
-		    
-		    i = search(EA(cskind) % V, energy)
-		    
-		    if (i .EQ. 0) then
-		        getac % N = 1
-		    else
-		        getac % N = A(cskind) % NY + 1
-		    end if
-		    
-		    allocate(getac % V(getac % N))
-		    getac % V(1) = 1.0
-		    fc = (energy - EA(cskind) % V(i)) / (EA(cskind) % V(i + 1) - EA(cskind) % V(i))
-		    do j = 2, getac % N
-		        getac % V(j) = A(cskind) % V(i, j - 1) +  &
-		              (A(cskind) % V(i + 1, j - 1) - A(cskind) % V(i, j - 1)) * fc 
-		    end do
-		    
-		end function getac
+		integer :: i
 		
-		! search the max index in array X so X(i)<=V
-		! return 0 if V<X(1) or V>=X(N), where N - length of X
-		function search(X, V)
-		    real(8), dimension(:), intent(in) :: X
-			real(8), intent(in) :: V
-			integer(4) :: search
-			
-			integer(4) :: imin, imax, imid, N
-			N = size(X)
-			if (V .LT. X(1) .OR. V .GE. X(N)) then
-			    search = 0
-			else
-			    imin = 1
-				imax = N
-				do while (imax - imin .GT. 1)
-				    imid = (imin + imax) / 2
-					if (X(imid) .LE. V) then
-					    imin = imid
-					else
-					    imax = imid
-					end if
-				end do
-				search = imin
-			end if
-			
-		end function search
+		i = search(E(cskind) % V, energy)
+		if (i .EQ. 0) then
+		    getcs = 0.0
+		else
+		    getcs = CS(cskind) % V(i) + (CS(cskind) % V(i + 1) - &
+			        CS(cskind) % V(i)) * (energy - E(cskind) % V(i)) / &
+			        (E(cskind) % V(i + 1) - E(cskind) % V(i))
+		end if
+		
+	end function getcs
+	
+	function getac(cskind, energy) ! linear interpolation used here. 
+	    integer(4), intent(in) :: cskind  ! If energy lies outside avaible crossection data range then
+	    real(8),    intent(in) :: energy  ! getac structure returned: getac%N=1, getac%V(1)=1.0 - uniform distribution is assumed
+	    type (VECTOR) :: getac
+	    
+	    integer :: i, j
+	    real(8) :: fc
+	    
+	    i = search(EA(cskind) % V, energy)
+	    
+	    if (i .EQ. 0) then
+	        getac % N = 1
+	    else
+	        getac % N = A(cskind) % NY + 1
+	    end if
+	    
+	    allocate(getac % V(getac % N))
+	    getac % V(1) = 1.0
+	    fc = (energy - EA(cskind) % V(i)) / (EA(cskind) % V(i + 1) - EA(cskind) % V(i))
+	    do j = 2, getac % N
+	        getac % V(j) = A(cskind) % V(i, j - 1) +  &
+	              (A(cskind) % V(i + 1, j - 1) - A(cskind) % V(i, j - 1)) * fc 
+	    end do
+	    
+	end function getac
+		
+	! search the max index in array X so X(i)<=V
+	! return 0 if V<X(1) or V>=X(N), where N - length of X
+	function search(X, V)
+	    real(8), dimension(:), intent(in) :: X
+		real(8), intent(in) :: V
+		integer(4) :: search
+		
+		integer(4) :: imin, imax, imid, N
+		N = size(X)
+		if (V .LT. X(1) .OR. V .GE. X(N)) then
+		    search = 0
+		else
+		    imin = 1
+			imax = N
+			do while (imax - imin .GT. 1)
+			    imid = (imin + imax) / 2
+				if (X(imid) .LE. V) then
+				    imin = imid
+    			else
+				    imax = imid
+				end if
+			end do
+			search = imin
+		end if
+		
+	end function search
 end module
--- kernel_prep.f90
+++ kernel_prep.f90
@@ -1,13 +1,40 @@
 module kernel_prep
 ! This module allows to calculate new kernel function or 
 ! download it from an existing file.
-
+!
+! S U B R O U T I N E S
+!
+!   kernel_init(typ, filename_ker, filename_cs) - initialize kernel. typ - kind of possible ways
+!              to initialize kernel. Possible values: integer NEW_KERNEL = 0 or OLD_KERNEL = 1.
+!              In the first case a new kernel is generated using crossection data from file filename_cs.
+!              File filename_ker contains initializing information like Np, N1, E1max, E1min, Epmax,
+!              Epmin, NAT then. In the last case already generated kernel, stored in file folename_ker
+!              is used. In this case filename_cs parameter is not required.
+!
+!   kernel_free(filename_ker) - free allocated memory when KERN is not required any more. Optional
+!              parameter filename_kernel - file name where generated kernel should be saved for
+!              further uses.
+!
+!   P A R A M E T E R S
+!
+!   KERN - kernel itself. This is matrix of Np x N1 dimensions. 
+!   E1 (len = N1) - mesh of radiation spectrum energy. 
+!   EP(len = Np) - mesh of response function (deposited) energy.
+!   NAT - the number of atoms in the detector
+!   dE1, dEP - corresponding mesh steps.
+!
+!   U S A G E
+!
+!   W = KERN * F
+!
+!   where F(E1) - radiation spectrum, W(EP) - response function
+!
     use crossections
 	
 	implicit none
 	
 	private
-	public :: KERN, E1, EP, dE1, dEP, kernel_init, kernel_free
+	public :: KERN, E1, EP, dE1, dEP, NEW_KERNEL, OLD_KERNEL, kernel_init, kernel_free
 	
 	type (TABLE)  :: KERN
 	type (VECTOR) :: E1, EP
@@ -15,125 +42,125 @@ module kernel_prep
 	integer, parameter :: NEW_KERNEL = 0, OLD_KERNEL = 1
 	real(8) :: dE1, dEP, NAT
 	
-    contains
+contains
 	
-	    subroutine kernel_init(typ, filename_ker, filename_cs)
-		    character(len = *), intent(in) :: filename_ker
-			character(len = *), optional, intent(in) :: filename_cs
-			integer, intent(in) :: typ
-			
-			integer :: N1, Np, error, i, j
-			real(8) :: E1min, E1max, Epmin, Epmax
-			
-			if (typ .EQ. OLD_KERNEL) then
-			    open(11, file = filename_ker, form = 'UNFORMATTED', action = 'READ')
-				read(11) N1, Np, NAT
-				E1 % N = N1
-				EP % N = Np
-				KERN % NX = Np
-				KERN % NY = N1
-				allocate(E1 % V(N1), EP % V(Np), KERN % V(Np, N1))
-				read(11) E1
-				read(11) EP
-				read(11) KERN
-				close(11)
-			else if (typ .EQ. NEW_KERNEL .AND. present(filename_cs)) then
-			    call cs_init(filename_cs, error)
-				open(11, file = filename_ker, form = 'UNFORMATTED', action = 'READ')
-				read(11) N1, Np, E1min, E1max, Epmin, Epmax, NAT
-				close(11)
-				E1 % N = N1;    Ep % N = Np
-     			KERN % NX = Np;	KERN % NY = N1
-                allocate(E1 % V(N1), EP % V(Np), KERN % V(Np, N1))
-				dE1 = (E1max - E1min) / N1
-				dEP = (Epmax - Epmin) / Np
-				do i = 1, N1
-				    E1 % V(i) = E1min + dE1 * (i - 0.5)
-				end do
-				do i = 1, Np
-				    EP % V(i) = Epmin + dEp * (i - 0.5)
-					do j = 1, N1
-					    KERN % V(i, j) = NAT * K(EP % V(i), E1 % V(j)) * dE1
+    subroutine kernel_init(typ, filename_ker, filename_cs)
+	    character(len = *), intent(in) :: filename_ker
+		character(len = *), optional, intent(in) :: filename_cs
+		integer, intent(in) :: typ
+		
+    	integer :: N1, Np, error, i, j
+		real(8) :: E1min, E1max, Epmin, Epmax
+		
+		if (typ .EQ. OLD_KERNEL) then
+		    open(11, file = filename_ker, form = 'UNFORMATTED', action = 'READ')
+			read(11) N1, Np, NAT
+			E1 % N = N1
+			EP % N = Np
+			KERN % NX = Np
+			KERN % NY = N1
+			allocate(E1 % V(N1), EP % V(Np), KERN % V(Np, N1))
+			read(11) E1
+			read(11) EP
+			read(11) KERN
+			close(11)
+		else if (typ .EQ. NEW_KERNEL .AND. present(filename_cs)) then
+		    call cs_init(filename_cs, error)
+			open(11, file = filename_ker, form = 'UNFORMATTED', action = 'READ')
+			read(11) N1, Np, E1min, E1max, Epmin, Epmax, NAT
+			close(11)
+			E1 % N = N1;    Ep % N = Np
+    		KERN % NX = Np;	KERN % NY = N1
+            allocate(E1 % V(N1), EP % V(Np), KERN % V(Np, N1))
+			dE1 = (E1max - E1min) / N1
+			dEP = (Epmax - Epmin) / Np
+			do i = 1, N1
+			    E1 % V(i) = E1min + dE1 * (i - 0.5)
+			end do
+			do i = 1, Np
+			    EP % V(i) = Epmin + dEp * (i - 0.5)
+				do j = 1, N1
+				    KERN % V(i, j) = NAT * K(EP % V(i), E1 % V(j)) * dE1
 					end do
-				end do
-			end if
-		end subroutine kernel_init
-		
-		subroutine kernel_free(filename_ker)
-		    character(len = *), optional, intent(in) :: filename_ker
-			
-			if (present(filename_ker)) then
-			    open(11, file = filename_ker, form = 'UNFORMATTED', action = 'WRITE')
-				write(11) E1 % N, EP % N, NAT
-                write(11) E1 % V
-                write(11) EP % V
-                write(11) KERN % V
-                close(11)				
-			end if
-			deallocate(E1 % V, EP % V, KERN % V)
-            call cs_destroy()		
-		end subroutine kernel_free
-	
-	    function K(Ep, E1)
-		    real(8), intent(in) :: Ep, E1
-			
-			integer :: i
-			
-			do i = 1, NReact
-			    K = K + getcs(i, E1) * f(i, mu(i, E1, Ep), E1) * dmu_dep(i, E1, Ep)
 			end do
-		end function K
+		end if
+	end subroutine kernel_init
+		
+	subroutine kernel_free(filename_ker)
+	    character(len = *), optional, intent(in) :: filename_ker
+		
+		if (present(filename_ker)) then
+		    open(11, file = filename_ker, form = 'UNFORMATTED', action = 'WRITE')
+			write(11) E1 % N, EP % N, NAT
+            write(11) E1 % V
+            write(11) EP % V
+            write(11) KERN % V
+            close(11)				
+	    end if
+		deallocate(E1 % V, EP % V, KERN % V)
+        call cs_destroy()		
+	end subroutine kernel_free
 	
-	    function f(r, mu, E)
-		    real(8), intent(in) :: mu, E
-			integer, intent(in) :: r
-			real(8) :: f
+	function K(Ep, E1)
+	    real(8), intent(in) :: Ep, E1
 			
-			integer l
-			type (VECTOR) :: a = getac(r, E)
+		integer :: i
 			
-			do l = 1, a % N
-			    f = f + (2 * l - 1) / 2 * a%V(l) * legendre(l - 1, mu)
-			end do
-			
-		end function f
+		do i = 1, NReact
+		    K = K + getcs(i, E1) * f(i, mu(i, E1, Ep), E1) * dmu_dep(i, E1, Ep)
+		end do
+	end function K
+	
+	function f(r, mu, E) ! anhular probability function for reaction r.
+	    real(8), intent(in) :: mu, E ! mu - cos of scattering angle, E - incident energy
+		integer, intent(in) :: r
+		real(8) :: f
 		
-		function mu(r, E1, Ep)
-		    integer, intent(in) :: r
-			real(8), intent(in) :: E1, Ep
-			
-			real(8) :: znamm, s2, s3
-			znam = sqrt(M2(r) * E1 * (M2(r) * E1 - QI(r) * (M1(r) + M2(r))))
-			s2 = (Ep(r) - QM(r) + QI(r)) / (2 * M1(r)) * (M1(r) + M2(r)) ** 2
-			s3 = QI(r) * (M1(r) + M2(r)) / 2
-			mu = (M2(r) * E1 - s2 - s3) / znam
-		end function mu
+		integer l
+		type (VECTOR) :: a = getac(r, E)
 		
-		function dmu_dep(r, E1, Ep)
-		    integer, intent(in) :: r
-			real(8), intent(in) :: E1, Ep
-			
-			real(8) :: znamm, s2
-			znam = sqrt(M2(r) * E1 * (M2(r) * E1 - QI(r) * (M1(r) + M2(r))))
-			s2 = (M1(r) + M2(r)) ** 2 / (2 * M1(r))
-			dmu_dep = s2 / znam		
-		end function dmu_dep
+		do l = 1, a % N
+		    f = f + (2 * l - 1) / 2 * a%V(l) * legendre(l - 1, mu)
+		end do
 		
-		recursive function legendre(n, x) result(p)
-		    integer, intent(in) :: n
-			real(8), intent(in) :: x
-			real(8) :: p
-			
-			if (n .EQ. 0) then
-			    p = 1
-			else if (n .EQ. 1) then
-			    p = x
-			else if (n .GT. 1) then
-			    p = ((2 * n - 1) * x * legendre(n - 1, x) - &
-				    (n - 1) * legendre(n - 2, x)) / n
-			else
-			    p = 1
-			end if
-		end function legendre
+	end function f
+	
+	function mu(r, E1, Ep) ! cosine of scattering angle as function of incedent and deposited energies.
+	    integer, intent(in) :: r
+		real(8), intent(in) :: E1, Ep
+		
+		real(8) :: znamm, s2, s3
+		znam = sqrt(M2(r) * E1 * (M2(r) * E1 - QI(r) * (M1(r) + M2(r))))
+		s2 = (Ep(r) - QM(r) + QI(r)) / (2 * M1(r)) * (M1(r) + M2(r)) ** 2
+		s3 = QI(r) * (M1(r) + M2(r)) / 2
+		mu = (M2(r) * E1 - s2 - s3) / znam
+	end function mu
+		
+	function dmu_dep(r, E1, Ep) ! d mu / d EP
+	    integer, intent(in) :: r
+		real(8), intent(in) :: E1, Ep
+		
+		real(8) :: znamm, s2
+		znam = sqrt(M2(r) * E1 * (M2(r) * E1 - QI(r) * (M1(r) + M2(r))))
+		s2 = (M1(r) + M2(r)) ** 2 / (2 * M1(r))
+		dmu_dep = s2 / znam		
+	end function dmu_dep
+		
+	recursive function legendre(n, x) result(p) ! legendre polynomials. Recursive calculation of
+	    integer, intent(in) :: n      ! legendre polynomials is used. This method is easy, and
+		real(8), intent(in) :: x      ! shouldn't affect considerably on efficiency, because
+		real(8) :: p                  ! in this problem n (order) is not very big
+		
+		if (n .EQ. 0) then
+		    p = 1
+		else if (n .EQ. 1) then
+		    p = x
+		else if (n .GT. 1) then
+		    p = ((2 * n - 1) * x * legendre(n - 1, x) - &
+			    (n - 1) * legendre(n - 2, x)) / n
+		else
+		    p = 1
+		end if
+	end function legendre
 
 end module kernel prep
